b8b5b61769d1d3b7b0fdcc305c72e0626f4986c6
Asmitapaudel <asmeetapdl@gmail.com>
2025-04-28 18:51:06 +0545

    add onclick function

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function forDisplay(value) {
+    document.calculator.display.value += value;
+}
+
+function clearDisplay() {
+    document.calculator.display.value = '';
+}
+
+function calculate() {
+    try {
+        document.calculator.display.value = eval(document.calculator.display.value);
+    } catch (e) {
+        document.calculator.display.value = 'Error';
+    }
+}

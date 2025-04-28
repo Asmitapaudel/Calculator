d41830f3577e8cf0c76ea25fc744765e776f0b05
Asmitapaudel <asmeetapdl@gmail.com>
2025-04-28 19:03:20 +0545

    done for a cleardisplay and calculate

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function forDisplay(value) {
     document.calculator.display.value += value;
 }
+function clearDisplay() {
+    document.calculator.display.value = '';
+}
+
+function calculate() {
+    try {
+        document.calculator.display.value = eval(document.calculator.display.value);
+    } catch (e) {
+        document.calculator.display.value = 'Error';
+    }
+}
